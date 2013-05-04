c32182d71f82ec8dee0b998640cef6d46e71957b
Jon Cheng <jon.syndicate@gmail.com>
2013-05-04 14:08:09 -0400

    added a circle to the map

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ L.tileLayer('http://{s}.tile.cloudmade.com/' + API_KEY + '/997/256/{z}/{x}/{y}.p
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
+
+var circle = L.circle([52.2100, 0.1300], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
