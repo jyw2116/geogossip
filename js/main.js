22bff07ade86338faabf5ed871c738fcc0631ac2
Jiyeon Woo <jiyeon8497@gmail.com>
2013-05-04 14:05:12 -0400

    add leaflet js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+var map = L.map('map').setView([52.2100, 0.1300], 13);
 
+L.tileLayer('http://{s}.tile.cloudmade.com/' + API_KEY + '/997/256/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map);
