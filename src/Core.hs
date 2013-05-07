2d7924acdb13579f9b566693a39540327068eadf
Daniel Choi <dhchoi@gmail.com>
2013-05-06 20:50:19 -0400

    some basic HDBC methods

--- src/Core.hs
+++ src/Core.hs
@@ -30,9 +30,24 @@ data Message = Message {
       messageId :: Int
     , messageText :: Text
     , messageUserNick :: Text
-    }
+    } deriving (Generic, Show)
 
 conn :: IO Connection
 conn = connectPostgreSQL "host=localhost dbname=geogossip2"
 
- 
+-- TODO enforce uniqueness requirement
+
+createUser :: String -> IO User
+createUser nick = do
+    c <- conn
+    [[id, nick]] <- quickQuery' c "insert into users (user_nick) values (?) returning user_id, user_nick" [toSql nick]
+    commit c
+    return $ User (fromSql id) (fromSql nick) Nothing
+
+updateUser :: String -> IO User
+updateUser nick = do
+    c <- conn
+    [[id, nick]] <- quickQuery' c "update users set user_nick = ? returning user_id, user_nick" [toSql nick]
+    commit c
+    return $ User (fromSql id) (fromSql nick) Nothing
+  
