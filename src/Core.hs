e50ba36fbaa5661904a630cdc4fa1e85d6cfc2d5
Daniel Choi <dhchoi@gmail.com>
2013-05-06 20:33:14 -0400

    sketching core haskell types

---
+++ src/Core.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
+module Core where
+import GHC.Generics (Generic)
+import Data.Text (Text)
+import qualified Data.Text as T
+
+import Database.HDBC
+import Database.HDBC.PostgreSQL
+
+type Lat = Double
+type Lng = Double
+type LatLng = (Lat, Lng)
+type Bounds = (LatLng, LatLng)  -- SW NE
+
+data Map = Map { 
+      mapId :: Int
+    , mapCenter :: LatLng,
+    , mapBounds :: Bounds
+    } deriving (Generic, Show)
+
+data Channel = Channel { 
+      channelId :: Int 
+    , channelCenter :: LatLng
+    , members :: [User]
+    } deriving (Generic, Show)
+
+data User = User { 
+      userId  :: Int 
+    , userNick :: Text
+    , userChannelId :: Maybe Int
+    } deriving (Generic, Show)
+    
+    
+
+conn :: IO Connection
+conn = connectPostgreSQL "host=localhost dbname=geogossip2"
+
+ 
