aa665ca63030d70c7e2a14f2b3ab027eb8424783
Daniel Choi <dhchoi@gmail.com>
2013-05-11 08:31:33 -0400

    more angular.js + d3 exercise code

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,6 @@
 
 
-
-
 var data = [32, 57, 112],
-    dataEnter = data.concat(293),
-    dataExit = data.slice(0, 2),
     w = 400,
     h = 300,
     x = d3.scale.ordinal().domain([57, 32, 112]).rangePoints([0, w], 1),
@@ -13,13 +9,12 @@ var data = [32, 57, 112],
 var svg = d3.select("#d3-1")
   .append("svg")
     .attr("width", "400px")
-    .attr("height", "300px")
-    .attr("background-color", "red");
+    .attr("height", "300px");
+
 
 svg.selectAll("circle")
     .data(data)
   .enter().append("circle")
-    .attr("class", "little")
     .attr("cx", x)
     .attr("cy", y)
     .attr("r", 12)
@@ -29,17 +24,23 @@ svg.selectAll("circle")
     .style("stroke-width", 3);
 
 
-
 /* Angular.js controller for the page */
 
 function ChatCtrl($scope, $http) {
     
     $scope.circleProps = { 
       radius:  12, 
-      fill: "#CCC",
+      fill: "steelblue",
       stroke: "red"
     };
 
+    $scope.users = [ 
+        {name: 'jon', x: 130, y: 130},
+        {name: 'jiyeon', x: 150, y: 150}, 
+        {name: 'judy', x:159, y: 120}
+    ];
+
+    /* watch the $scope.circleProps object for changes, including initialization */
     $scope.$watch('circleProps.radius', function (newVal, oldVal) {
         if (parseInt(newVal) > 0) {
           svg.selectAll("circle").attr("r", newVal);
@@ -52,26 +53,46 @@ function ChatCtrl($scope, $http) {
         svg.selectAll("circle").style("stroke", newVal);
     });
 
+    /* watch the $scope.users array for changes, including initialization */
+    $scope.$watch('users', function (newVal, oldVal) {
+        var users = newVal;
+        $scope.plotUsers();
+    }, true);
+
   
     $scope.pulse = function() {
-
-      svg.selectAll("cicle")
-          .data(data)
-        .enter().append("circle")
-          .attr("class", "select")
-          .attr("cx", x)
-          .attr("cy", y)
-          .attr("r", $scope.circleProps.radius * 3)
-          .style("fill", "none")
-          .style("stroke", $scope.circleProps.stroke)
-          .style("stroke-opacity", 1e-6)
-          .style("stroke-width", 3)
-        .transition()
-          .duration(750)
-          .attr("r", $scope.circleProps.radius)
-          .style("stroke-opacity", 1);
-
-
+        svg.selectAll("cicle")
+            .data(data)
+          .enter().append("circle")
+            .attr("cx", x)
+            .attr("cy", y)
+            .attr("r", $scope.circleProps.radius * 3)
+            .style("fill", "none")
+            .style("stroke", $scope.circleProps.stroke)
+            .style("stroke-opacity", 1e-6)
+            .style("stroke-width", 3)
+          .transition()
+            .duration(750)
+            .attr("r", $scope.circleProps.radius)
+            .style("stroke-opacity", 1);
     }
+
+    $scope.plotUsers = function() {
+        svg.selectAll(".nameBubble").remove();
+        svg.selectAll(".nameBubble")
+          .data($scope.users)
+        .enter().append("g")
+          .attr("class", "nameBubble")
+          .append("text")
+          .attr("text-anchor", "middle")
+          .text(function(d) {return d.name})
+          .attr("x", function(d) {return d.x})
+          .attr("y", function(d) {return d.y});
+      
+    }
+
+    $scope.addUser = function() {
+        $scope.users.push({});
+    };
 }
 
